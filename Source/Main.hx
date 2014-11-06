30ee94b37aa804236f7d397cfbbe0a834bf0e6e7
Valentin Lemière <me@ibilon.com>
2014-11-06 19:01:34 +0100

    Modularization

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,9 +1,8 @@
 package;
 
-import openfl.display.*;
-import openfl.geom.*;
-import openfl.gl.*;
-import openfl.utils.*;
+import openfl.display.Bitmap;
+import openfl.display.OpenGLView;
+import openfl.display.Sprite;
 import openfl.Assets;
 
 class Main extends Sprite {
@@ -16,25 +15,50 @@ class Main extends Sprite {
 
 		if (OpenGLView.isSupported) {
 
+			ShaderCompositing.init (400, 400);
+
+			var c1 = ShaderCompositing.uploadLayers ([Assets.getBitmapData ("assets/c1_bottom.png"), Assets.getBitmapData ("assets/c1_middle.png"), Assets.getBitmapData ("assets/c1_top.png")]);
+			var c2 = ShaderCompositing.uploadLayers ([Assets.getBitmapData ("assets/c2_bottom.png"), Assets.getBitmapData ("assets/c2_middle.png"), Assets.getBitmapData ("assets/c2_top.png")]);
+
 			var shader = "varying vec2 vTexCoord;
 			uniform sampler2D uImage0;
-			
-			void main(void)
+
+			void main (void)
 			{
 				vec4 color = texture2D (uImage0, vTexCoord);
 				gl_FragColor = vec4 (1 - color.r, 1 - color.g, 1 - color.b, color.a);
 			}";
 
-			var source = Assets.getBitmapData ("assets/openfl.png");
-			var result = applyShader (source, shader);
+			var c1_bitmap = new Bitmap (ShaderCompositing.composite (c1, shader));
+			var c2_bitmap = new Bitmap (ShaderCompositing.composite (c2, shader));
 
-			var bitmap_source = new Bitmap (source);
-			addChild (bitmap_source);
+			addChild (c1_bitmap);
+			addChild (c2_bitmap);
+			c2_bitmap.x = c1_bitmap.width + 20;
 
-			var bitmap_result = new Bitmap (result);
-			addChild (bitmap_result);
+			var shader = "varying vec2 vTexCoord;
+			uniform sampler2D uImage0;
 
-			bitmap_result.x = bitmap_source.width;
+			void main (void)
+			{
+				vec4 color = texture2D (uImage0, vTexCoord);
+				float c = 0.3*color.r + 0.59*color.g + 0.11*color.b;
+				gl_FragColor = vec4 (c, c, c, color.a);
+			}";
+
+			var c1_bitmap = new Bitmap (ShaderCompositing.composite (c1, shader));
+			var c2_bitmap = new Bitmap (ShaderCompositing.composite (c2, shader));
+
+			addChild (c1_bitmap);
+			c1_bitmap.y = 420;
+			addChild (c2_bitmap);
+			c2_bitmap.y = 420;
+			c2_bitmap.x = c1_bitmap.width + 20;
+
+			c1.delete ();
+			c2.delete ();
+
+			ShaderCompositing.clean ();
 
 		} else {
 
@@ -44,200 +68,6 @@ class Main extends Sprite {
 
 	}
 
-	public static function applyShader (source:BitmapData, fragmentSource:String) : BitmapData {
-
-		var width = source.width;
-		var height = source.height;
-		var size = width * height * 4;
-		source = source.clone();
-
-		// FrameBuffer construction
-
-		var texture = GL.createTexture();
-        GL.bindTexture(GL.TEXTURE_2D, texture);
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.REPEAT);
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.REPEAT);
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST);
-        GL.texParameteri(GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
-
-        GL.texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, width, height, 0, GL.RGBA, GL.UNSIGNED_BYTE, null);
-
-        var framebuffer = GL.createFramebuffer();
-        GL.bindFramebuffer(GL.FRAMEBUFFER, framebuffer);
-
-        GL.framebufferTexture2D(GL.FRAMEBUFFER, GL.COLOR_ATTACHMENT0, GL.TEXTURE_2D, texture, 0);
-
-        var renderBuffer = GL.createRenderbuffer();
-        GL.bindRenderbuffer(GL.RENDERBUFFER, renderBuffer);
-        GL.renderbufferStorage(GL.RENDERBUFFER, GL.DEPTH_COMPONENT, width, height);
-
-        GL.framebufferRenderbuffer(GL.FRAMEBUFFER, GL.DEPTH_ATTACHMENT, GL.RENDERBUFFER, renderBuffer);
-
-        var status = GL.checkFramebufferStatus(GL.FRAMEBUFFER);
-        switch(status)
-        {
-            case GL.FRAMEBUFFER_COMPLETE:
-            
-            default:
-				trace ("FrameBuffer error");
-				return null;
-        }
-        
-        // Draw
-        
-        var vertexSource = 
-					
-			"attribute vec4 aPosition;
-			attribute vec2 aTexCoord;
-			varying vec2 vTexCoord;
-			
-			uniform mat4 uMatrix;
-			
-			void main(void) {
-				
-				vTexCoord = aTexCoord;
-				gl_Position = uMatrix * aPosition;
-				
-			}";
-		
-		var program = GLUtils.createProgram (vertexSource, fragmentSource);
-		GL.useProgram (program);
-		
-		var vertexAttribute = GL.getAttribLocation (program, "aPosition");
-		var textureAttribute = GL.getAttribLocation (program, "aTexCoord");
-		var matrixUniform = GL.getUniformLocation (program, "uMatrix");
-		var imageUniform = GL.getUniformLocation (program, "uImage0");
-		
-		GL.enableVertexAttribArray (vertexAttribute);
-		GL.enableVertexAttribArray (textureAttribute);
-		GL.uniform1i (imageUniform, 0);
-		
-		var data = [
-			
-			width, height, 0, 1, 1,
-			0, height, 0, 0, 1,
-			width, 0, 0, 1, 0,
-			0, 0, 0, 0, 0
-			
-		];
-		
-		var buffer = GL.createBuffer ();
-		GL.bindBuffer (GL.ARRAY_BUFFER, buffer);
-		GL.bufferData (GL.ARRAY_BUFFER, new Float32Array (cast data), GL.STATIC_DRAW);
-		GL.bindBuffer (GL.ARRAY_BUFFER, null);
-		
-		var textureDisplay = GL.createTexture ();
-		GL.bindTexture (GL.TEXTURE_2D, textureDisplay);
-		GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
-		GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE);
-		GL.texImage2D (GL.TEXTURE_2D, 0, GL.RGBA, width, height, 0, GL.RGBA, GL.UNSIGNED_BYTE, new UInt8Array (BitmapData.getRGBAPixels (source)));
-		GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
-		GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
-		GL.bindTexture (GL.TEXTURE_2D, null);
-		
-		GL.viewport (0, 0, width, height);
-		
-		GL.clearColor (0.0, 0.0, 0.0, 1.0);
-		GL.clear (GL.COLOR_BUFFER_BIT);
-		
-		var matrix = Matrix3D.createOrtho (0, width, height, 0, -1000, 1000);
-		GL.uniformMatrix4fv (matrixUniform, false, Float32Array.fromMatrix(matrix));
-		
-		GL.activeTexture (GL.TEXTURE0);
-		GL.bindTexture (GL.TEXTURE_2D, textureDisplay);
-		GL.enable (GL.TEXTURE_2D);
-		
-		GL.bindBuffer (GL.ARRAY_BUFFER, buffer);
-		GL.vertexAttribPointer (vertexAttribute, 3, GL.FLOAT, false, 5 * Float32Array.SBYTES_PER_ELEMENT, 0);
-		GL.vertexAttribPointer (textureAttribute, 2, GL.FLOAT, false, 5 * Float32Array.SBYTES_PER_ELEMENT, 3 * Float32Array.SBYTES_PER_ELEMENT);
-		
-		GL.drawArrays (GL.TRIANGLE_STRIP, 0, 4);
-
-        // Get result
-         
-        var result = new ByteArray (size);
-        GL.readPixels(0, 0, width, height, GL.RGBA, GL.UNSIGNED_BYTE, result);
-        
-		// Clean up
-
-        GL.deleteTexture(texture);
-        GL.deleteTexture(textureDisplay);
-        GL.deleteRenderbuffer(renderBuffer);
-        GL.deleteBuffer(buffer);
-
-        GL.bindFramebuffer(GL.FRAMEBUFFER, null);
-        GL.deleteFramebuffer(framebuffer);
-        
-        // Make final BitmapData
-		
-		var res = new BitmapData(width, height, true);
-		
-		var index = 0;
-		for (y in 0...height)
-		{
-			for (x in 0...width)
-			{
-				var r = result.readUnsignedByte();
-				var g = result.readUnsignedByte();
-				var b = result.readUnsignedByte();
-				var a = result.readUnsignedByte();
-				
-				var color = (a << 24) + (r << 16) + (g << 8) + b;				
-				res.setPixel32(x, height - y - 1, color);
-			}
-		}
-		
-		return res;
-
-	}
-
 }
 
-class GLUtils {
-	
-	
-	public static function compileShader (source:String, type:Int):GLShader {
-		
-		var shader = GL.createShader (type);
-		GL.shaderSource (shader, source);
-		GL.compileShader (shader);
-		
-		if (GL.getShaderParameter (shader, GL.COMPILE_STATUS) == 0) {
-			
-			switch (type) {
-				
-				case GL.VERTEX_SHADER: throw "Error compiling vertex shader";
-				case GL.FRAGMENT_SHADER: throw "Error compiling fragment shader";
-				default: throw "Error compiling unknown shader type";
-				
-			}
-			
-		}
-		
-		return shader;
-		
-	}
-	
-	
-	public static function createProgram (vertexSource:String, fragmentSource:String):GLProgram {
-		
-		var vertexShader = compileShader (vertexSource, GL.VERTEX_SHADER);
-		var fragmentShader = compileShader (fragmentSource, GL.FRAGMENT_SHADER);
-		
-		var program = GL.createProgram ();
-		GL.attachShader (program, vertexShader);
-		GL.attachShader (program, fragmentShader);
-		GL.linkProgram (program);
-		
-		if (GL.getProgramParameter (program, GL.LINK_STATUS) == 0) {
-			
-			throw "Unable to initialize the shader program.";
-			
-		}
-		
-		return program;
-		
-	}
-	
-	
-}
+
---
+++ Source/ShaderCompositing.hx
@@ -0,0 +1,266 @@
+import openfl.display.BitmapData;
+import openfl.geom.Matrix3D;
+import openfl.gl.*;
+import openfl.utils.*;
+
+class ShaderCompositing {
+
+	private static var fb_texture : GLTexture;
+	private static var fb_framebuffer : GLFramebuffer;
+	private static var fb_renderbuffer : GLRenderbuffer;
+	private static var fb_vertexShader : GLShader;
+
+	public static function init (maxWidth:Int, maxHeight:Int) : Void {
+
+		fb_texture = GL.createTexture ();
+        GL.bindTexture (GL.TEXTURE_2D, fb_texture);
+        GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.REPEAT);
+        GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.REPEAT);
+        GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.NEAREST);
+        GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.NEAREST);
+
+        GL.texImage2D (GL.TEXTURE_2D, 0, GL.RGBA, maxWidth, maxHeight, 0, GL.RGBA, GL.UNSIGNED_BYTE, null);
+
+        fb_framebuffer = GL.createFramebuffer ();
+        GL.bindFramebuffer (GL.FRAMEBUFFER, fb_framebuffer);
+
+        GL.framebufferTexture2D (GL.FRAMEBUFFER, GL.COLOR_ATTACHMENT0, GL.TEXTURE_2D, fb_texture, 0);
+
+        fb_renderbuffer = GL.createRenderbuffer();
+        GL.bindRenderbuffer (GL.RENDERBUFFER, fb_renderbuffer);
+        GL.renderbufferStorage (GL.RENDERBUFFER, GL.DEPTH_COMPONENT, maxWidth, maxHeight);
+
+        GL.framebufferRenderbuffer (GL.FRAMEBUFFER, GL.DEPTH_ATTACHMENT, GL.RENDERBUFFER, fb_renderbuffer);
+
+        var status = GL.checkFramebufferStatus (GL.FRAMEBUFFER);
+        switch (status) {
+
+            case GL.FRAMEBUFFER_COMPLETE:
+
+            default:
+				throw ("FrameBuffer error");
+
+        }
+
+        GL.bindFramebuffer (GL.FRAMEBUFFER, null);
+
+        var vertexSource =
+
+			"attribute vec4 aPosition;
+			attribute vec2 aTexCoord;
+			varying vec2 vTexCoord;
+
+			uniform mat4 uMatrix;
+
+			void main (void) {
+
+				vTexCoord = vec2 (aTexCoord.x, 1.0-aTexCoord.y); // flip y
+				gl_Position = uMatrix * aPosition;
+
+			}";
+
+		fb_vertexShader = GLUtils.compileShader (vertexSource, GL.VERTEX_SHADER);
+
+	}
+
+	public static function uploadLayers (layers:Array<BitmapData>) : LayerGroup {
+
+		var textures = new Array<GLTexture>();
+
+		for (layer in layers) {
+
+			var texture = GL.createTexture ();
+
+			GL.bindTexture (GL.TEXTURE_2D, texture);
+			GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_WRAP_S, GL.CLAMP_TO_EDGE);
+			GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_WRAP_T, GL.CLAMP_TO_EDGE);
+			GL.texImage2D (GL.TEXTURE_2D, 0, GL.RGBA, layer.width, layer.height, 0, GL.RGBA, GL.UNSIGNED_BYTE, new UInt8Array (BitmapData.getRGBAPixels (layer.clone())));
+			GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_MAG_FILTER, GL.LINEAR);
+			GL.texParameteri (GL.TEXTURE_2D, GL.TEXTURE_MIN_FILTER, GL.LINEAR);
+			GL.bindTexture (GL.TEXTURE_2D, null);
+
+			textures.push (texture);
+
+		}
+
+		return new LayerGroup (textures, layers[0].width, layers[0].height);
+
+	}
+
+	public static function composite (group:LayerGroup, fragmentShader:String) : BitmapData {
+
+		GL.bindFramebuffer (GL.FRAMEBUFFER, fb_framebuffer);
+
+		var program = GLUtils.createProgram (fb_vertexShader, fragmentShader);
+		GL.useProgram (program);
+
+		var vertexAttribute = GL.getAttribLocation (program, "aPosition");
+		var textureAttribute = GL.getAttribLocation (program, "aTexCoord");
+		var matrixUniform = GL.getUniformLocation (program, "uMatrix");
+		var imageUniform = GL.getUniformLocation (program, "uImage0");
+
+		GL.enableVertexAttribArray (vertexAttribute);
+		GL.enableVertexAttribArray (textureAttribute);
+		GL.uniform1i (imageUniform, 0);
+
+		GL.viewport (0, 0, group.width, group.height);
+
+		GL.clearColor (0.0, 0.0, 0.0, 1.0);
+		GL.clear (GL.COLOR_BUFFER_BIT);
+
+		GL.enable (GL.BLEND);
+		GL.blendFunc (GL.SRC_ALPHA, GL.ONE_MINUS_SRC_ALPHA);
+
+		GL.uniformMatrix4fv (matrixUniform, false, group.matrix);
+
+		GL.bindBuffer (GL.ARRAY_BUFFER, group.buffer);
+		GL.vertexAttribPointer (vertexAttribute, 3, GL.FLOAT, false, 5 * Float32Array.SBYTES_PER_ELEMENT, 0);
+		GL.vertexAttribPointer (textureAttribute, 2, GL.FLOAT, false, 5 * Float32Array.SBYTES_PER_ELEMENT, 3 * Float32Array.SBYTES_PER_ELEMENT);
+
+		for (texture in group.textures) {
+
+			GL.activeTexture (GL.TEXTURE0);
+			GL.bindTexture (GL.TEXTURE_2D, texture);
+			GL.enable (GL.TEXTURE_2D);
+
+			GL.drawArrays (GL.TRIANGLE_STRIP, 0, 4);
+
+		}
+
+		var size = group.width * group.height;
+
+        var result_rgba = new ByteArray (size*4);
+        GL.readPixels (0, 0, group.width, group.height, GL.RGBA, GL.UNSIGNED_BYTE, result_rgba);
+
+        var result_argb = new ByteArray (size*4);
+		for (i in 0...size) {
+
+			var r = result_rgba.readUnsignedByte ();
+			var g = result_rgba.readUnsignedByte ();
+			var b = result_rgba.readUnsignedByte ();
+			var a = result_rgba.readUnsignedByte ();
+
+			result_argb.writeByte (a);
+			result_argb.writeByte (r);
+			result_argb.writeByte (g);
+			result_argb.writeByte (b);
+
+		}
+		result_argb.position = 0;
+
+		var res = new BitmapData (group.width, group.height, true);
+		res.setPixels (res.rect, result_argb);
+
+		GL.bindFramebuffer (GL.FRAMEBUFFER, null);
+		GL.bindBuffer (GL.ARRAY_BUFFER, null);
+		GL.deleteProgram (program);
+
+		return res;
+
+	}
+
+	public static function clean () : Void {
+
+		GL.deleteRenderbuffer (fb_renderbuffer);
+		GL.deleteFramebuffer (fb_framebuffer);
+		GL.deleteTexture (fb_texture);
+
+	}
+
+}
+
+class LayerGroup {
+
+	public var textures : Array<GLTexture>;
+	public var width : Int;
+	public var height : Int;
+	public var buffer : GLBuffer;
+	public var matrix : Float32Array;
+
+	public function new (textures:Array<GLTexture>, width:Int, height:Int) {
+
+		this.textures = textures;
+		this.width = width;
+		this.height = height;
+
+		var data = [
+
+			width, height, 0, 1, 1,
+			0, height, 0, 0, 1,
+			width, 0, 0, 1, 0,
+			0, 0, 0, 0, 0
+
+		];
+
+		buffer = GL.createBuffer ();
+		GL.bindBuffer (GL.ARRAY_BUFFER, buffer);
+		GL.bufferData (GL.ARRAY_BUFFER, new Float32Array (cast data), GL.STATIC_DRAW);
+		GL.bindBuffer (GL.ARRAY_BUFFER, null);
+
+		var matrix_3d = Matrix3D.createOrtho (0, width, height, 0, -1000, 1000);
+		matrix = Float32Array.fromMatrix (matrix_3d);
+
+	}
+
+	public function delete () : Void {
+
+		for (texture in textures) {
+
+			GL.deleteTexture (texture);
+
+		}
+
+		GL.deleteBuffer (buffer);
+
+	}
+
+}
+
+/**
+ * Modified lime.utils.GLUtils
+ * The MIT License (MIT) Copyright (c) 2013-2014 OpenFL contributors
+ */
+class GLUtils {
+
+	public static function compileShader (source:String, type:Int):GLShader {
+
+		var shader = GL.createShader (type);
+		GL.shaderSource (shader, source);
+		GL.compileShader (shader);
+
+		if (GL.getShaderParameter (shader, GL.COMPILE_STATUS) == 0) {
+
+			switch (type) {
+
+				case GL.VERTEX_SHADER: throw "Error compiling vertex shader";
+				case GL.FRAGMENT_SHADER: throw "Error compiling fragment shader";
+				default: throw "Error compiling unknown shader type";
+
+			}
+
+		}
+
+		return shader;
+
+	}
+
+	public static function createProgram (vertexShader:GLShader, fragmentSource:String):GLProgram {
+
+		var fragmentShader = compileShader (fragmentSource, GL.FRAGMENT_SHADER);
+
+		var program = GL.createProgram ();
+		GL.attachShader (program, vertexShader);
+		GL.attachShader (program, fragmentShader);
+		GL.linkProgram (program);
+
+		if (GL.getProgramParameter (program, GL.LINK_STATUS) == 0) {
+
+			throw "Unable to initialize the shader program.";
+
+		}
+
+		return program;
+
+	}
+
+}
